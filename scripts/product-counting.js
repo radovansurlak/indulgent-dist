c1867162095feb7b6cd0edba26cf462e91e1d0d7
radovansurlak <radovansurlak@gmail.com>
2016-12-14 14:32:01 +0100

    removed index.html

--- scripts/product-counting.js
+++ scripts/product-counting.js
@@ -1,32 +1,32 @@
 
 var productCount = {
-  pumpkin: parseInt(Cookies.get("pumpkin")),
-  vanilla: parseInt(Cookies.get("vanilla")),
-  pepermint: parseInt(Cookies.get("pepermint"))
+  pumpkin: parseInt(Cookies.get('pumpkin')),
+  vanilla: parseInt(Cookies.get('vanilla')),
+  pepermint: parseInt(Cookies.get('pepermint'))
 };
 
-$("#cart-count").html(Cookies.get("productSum"));
+$('#cart-count').html(Cookies.get('productSum'));
 
 $('button[type="buy-now"]').click(function () {
 
   var selected = $(this).parent().parent().attr('id');
 
-  if (selected == "pumpkin") {
+  if (selected == 'pumpkin') {
     productCount.pumpkin++;
-    Cookies.set("pumpkin", productCount.pumpkin);
-  } else if (selected == "vanilla") {
+    Cookies.set('pumpkin', productCount.pumpkin);
+  } else if (selected == 'vanilla') {
     productCount.vanilla++;
-    Cookies.set("vanilla", productCount.vanilla);
+    Cookies.set('vanilla', productCount.vanilla);
   } else {
     productCount.pepermint++;
-    Cookies.set("pepermint", productCount.pepermint);
+    Cookies.set('pepermint', productCount.pepermint);
   };
 
   var productSum = productCount.pumpkin + productCount.vanilla + productCount.pepermint;
 
-  Cookies.set("productSum", productSum);
+  Cookies.set('productSum', productSum);
 
-  $("#cart-count").html(Cookies.get("productSum"));
+  $('#cart-count').html(Cookies.get('productSum'));
 
   console.log(productCount);
 });
